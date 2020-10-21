10ed90ec6dd8c8abe5470af60446ca67651ede88
akhileshgowda7 <50296948+akhileshgowda7@users.noreply.github.com>
2020-10-21 10:58:46 +0530

    Add files via upload
    This application is called the Sentence Maker. it asks the user to say something repeatedly until the enter \end. All the sentences that they type till the \end will be displayed continuously separated by . and capitalising the first letter of the sentence and its adds a ? if the sentence is a question

---
+++ sentencemaker.py
@@ -0,0 +1,16 @@
+def sentence_maker(phrase):
+    interogatives = ("how","what","why")
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+results = []
+while True:
+     user_input=input("say something : ")
+     if user_input == "\end":
+        break
+     else:
+          results.append(sentence_maker(user_input))
+print("".join(results))
+        
