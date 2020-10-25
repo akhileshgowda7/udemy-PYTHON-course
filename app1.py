b94ee7ba86d4f592e37a601e21b4640e634cec26
akhileshgowda7 <50296948+akhileshgowda7@users.noreply.github.com>
2020-10-25 18:26:50 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,12 @@
 import json
 from difflib import get_close_matches
-
 data = json.load(open("data.json"))
-
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
@@ -25,3 +25,4 @@ if type(output) == list:
         print(item)
 else:
     print(output)
+
